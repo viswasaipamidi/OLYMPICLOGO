63590d54e93b93804c9784832ca57ae032d0826e
viswasaipamidi <89049847+viswasaipamidi@users.noreply.github.com>
2022-09-17 12:05:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 canvas = document.getElementById("myCanvas");
 ctx= canvas.getContext("2d");
 
-    Use beginPath() function to begin a path.
+/*    Use beginPath() function to begin a path.
     Then, use strokeStyle to set the color to "grey".
     Use lineWidth and set the width to 1.
     Use rect() to create a rectangle at 150 and 143
@@ -21,7 +21,6 @@ ctx.stroke();
     Use arc() to create a circle at x = 250 and y = 210
 with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
 */
-
 ctx.beginPath();
 ctx.strokeStyle="blue";
 ctx.lineWidth= 5;
